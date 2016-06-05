ce43d8a57d4d97f65700898660f7c82537341ea2
Brian Coutinho <coutinho@rockhopper-05.cs.wisc.edu>
2016-06-05 17:59:06 -0500

    Adding a simple 2D sampling script for laying out the basic flow-chart

--- configs/myconfigs/sample_2d.py
+++ configs/myconfigs/sample_2d.py
@@ -25,15 +25,26 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
-# Authors: Jason Lowe-Power
+# Authors: Jason Lowe-Power, Brian Coutinho
 
-""" An example of how to use gem5+kvm for statistical sampling.
+""" This script is an example of multi-threaded statistical sampling
+    where we simulate a sample multiple times to cover several possible
+    thread interleavings. Random pertubations are added during the
+    simulation to exercise mutliple execution paths/interleavings.
 
-    This script takes two arguments, the total number of instructions in the
-    ROI (which you can find by using runkvm.py) and the number of sample
-    points you want to take during the execution.
+    The sampling process thus works as two nested for-loops,
+    the outer one for each sample point in the program and
+    the inner one for mulitple observations at each sample point.
 
-    The output is a set of directories in the outdir, one per sample point.
+    This script takes three arguments, the total number of instructions in the
+    ROI (which you can find by using runkvm.py), the number of sample
+    points you want to take during the execution, and the number of runs
+    to make at each sample point.
+
+    The output is a set of two levels of directories in the outdir.
+    At the top level is directory for each sample point which in turn
+    contains multiple sub-directories for each random observation.
+
 """
 
 import os
@@ -50,108 +61,14 @@ import SimpleOpts
 
 from system import MySystem
 
-SimpleOpts.add_option("--script", default='',
-                      help="Script to execute in the simulated system")
-SimpleOpts.add_option("--warmup_time", default="10ms",
-                      help="Simulated functional warmup time in ms")
-SimpleOpts.add_option("--detailed_warmup_time", default="10us",
-                      help="Simulated detailed warmup time in ms")
-SimpleOpts.add_option("--detailed_time", default="1ms",
-                      help="Simulated time in ms for detailed measurements, \
-                          (size of measurment window)")
+sys.path.append('configs/myconfigs/') # For the next line...
+from sample import fastforward, runSim, warmupAndRun
 
-SimpleOpts.set_usage("usage: %prog [options] roi_instructions samples=1")
-
-class UnexpectedExit(Exception):
-    pass
-
-def fastforward(system, instructions):
-    """ Fast forward the simulation by instructions.
-        This only works with system.cpu (kvm CPU), not other CPUs.
-        @param instructions the number of instructions to simulate before
-               exit. This is the total instructions across all cores.
-        @return The number of instructions past the goal
-    """
-    goal = system.totalInsts() + instructions
-
-    for i,cpu in enumerate(system.cpu):
-        if not hasattr(cpu, "_outstandingExit"):
-            cpu._outstandingExit = False
-        if not cpu._outstandingExit:
-            exit_inst = int(instructions/len(system.cpu))
-            cpu.scheduleInstStop(0, exit_inst, "Max Insts CPU %d" % (i))
-            cpu._outstandingExit = True
-
-    # There isn't a way to stop when we hit a specific global number of insts
-    # Therefore, we have to exit after each CPU executes X insts. When the
-    # first few CPUs exit, we won't have executed enough global insts, so
-    # keep going until we do.
-    while 1:
-        exit_event = m5.simulate()
-
-        if not exit_event.getCause().startswith("Max Insts"):
-            raise UnexpectedExit(exit_event.getCause())
-
-        cpu_id = int(exit_event.getCause().split(' ')[-1])
-        system.cpu[cpu_id]._outstandingExit = False
-
-        if system.totalInsts() > goal:
-            return system.totalInsts() - goal
-
-        exit_inst = min(int(instructions/len(system.cpu)),
-                        goal-system.totalInsts())
-        system.cpu[cpu_id].scheduleInstStop(0, exit_inst,
-                                            "Max Insts CPU %d" % (cpu_id))
-        system.cpu[cpu_id]._outstandingExit = True
+SimpleOpts.set_usage("usage: %prog [options] roi_instructions \
+samples=1 runs=1")
 
 
-def runSim(sim_time):
-    """ Run the simulation.
-        This will run the simulation ignoring exits for "Max Insts", but
-        raising an exception for all other exit types.
-        @param sim_time the amount of guest-time to simulate as a string
-               (e.g., "10ms")
-    """
-    ticks = fromSeconds(toLatency(sim_time))
-    abs_ticks = m5.curTick() + ticks
-
-    exit_event = m5.simulate(ticks)
-    while exit_event.getCause() != "simulate() limit reached":
-        # It's possible (likely) there are a few max insts exit events still
-        # scheduled. We should ignore these. See fastforward
-        print "Exited:", exit_event.getCause()
-        if not exit_event.getCause().startswith("Max Insts"):
-            raise UnexpectedExit(exit_event.getCause())
-
-        exit_event = m5.simulate(abs_ticks - m5.curTick())
-
-def warmupAndRun(system, warmup_time, detailed_warmup_time, sim_time):
-    """ Warmup and run the detailed simulation
-        This function first executes the system with the atomic CPU to warmup
-        the caches and branch predictors. Next, it runs in detailed warmup
-        mode with the detailed CPU to warmup the pipeline. Once the system is
-        warm, we reset the stats and start the detailed simulation.
-        NOTE: This function assumes the KVM CPU is currently active
-              (system.cpu). After this function the detailed CPU (timingCPU)
-              is active.
-
-        @param system the system we are running
-        @param warmup_time amount of guest-time to warmup
-        @param detailed_warmup_time amout of guest-time for detailed warming
-        @param sim_time amount of guest-time for detailed simulation
-    """
-    print "Warming up"
-    system.switchCpus(system.cpu, system.atomicCpu)
-    runSim(warmup_time)
-    print "Detailed warmup"
-    system.switchCpus(system.atomicCpu, system.timingCpu)
-    runSim(detailed_warmup_time)
-    print "Running the detailed simulation!"
-    # Reset the stats so we only record things in the detailed simulation
-    m5.stats.reset()
-    runSim(sim_time)
-
-def simulateROI(system, instructions, samples):
+def simulateROI(system, instructions, samples, runs):
     """ Do everything we need to do during the ROI.
         At a high-level, this function executes the entire ROI and takes
         a number of random sample points. At each point it may run multiple
@@ -161,6 +78,7 @@ def simulateROI(system, instructions, samples):
         @param instructions total instructions in the ROI (summed over all
                CPUs)
         @param samples number of sample points in the ROI
+        @param runs per sample point
     """
     from random import randint, sample, seed
     import signal
@@ -222,25 +140,31 @@ def simulateROI(system, instructions, samples):
         while len(pids) >= 4 - 1:
             time.sleep(1)
 
-        # Fork gem5 and get a new PID. Save the stats in a folder based on
-        # the instruction number.
-        pid = m5.fork('%(parent)s/'+str(insts))
-        if pid == 0: # in child
-            from m5.internal.core import seedRandom
-            # Make sure each instance of gem5 starts with a different
-            # random seed. Can't just use time, since this may occur
-            # multiple times in the same second.
-            rseed = int(time.time()) * os.getpid() * randint(0,10000)
-            seedRandom(rseed)
-            print "Running detailed simulation for sample", i
-            warmupAndRun(system, warmup_time , detailed_warmup_time, \
-                          detailed_time)
-            print "Done with detailed simulation for sample", i
-            # Just exit in the child. No need to do anything else.
-            sys.exit(0)
-        else: # in parent
-            # Append the child's PID and fast forward to the next point
-            pids.append(pid)
+        # Now that we have hit the sample point take multiple observations
+        parent = m5.options.outdir
+        os.mkdir('%(parent)s/'%{'parent':parent} + str(insts))
+
+        # Clone a new gem5 process for each observation
+        for r in range(runs):
+            # Fork gem5 and get a new PID. Save the stats in a folder based on
+            # the instruction number and subdir based on run no.
+            pid = m5.fork('%(parent)s/'+str(insts)+'/'+str(r))
+            if pid == 0: # in child
+                from m5.internal.core import seedRandom
+                # Make sure each instance of gem5 starts with a different
+                # random seed. Can't just use time, since this may occur
+                # multiple times in the same second.
+                rseed = int(time.time()) * os.getpid()
+                seedRandom(rseed)
+                print "Running detailed simulation for sample:", i, "run:", r
+                warmupAndRun(system, warmup_time , detailed_warmup_time, \
+                              detailed_time)
+                print "Done with detailed simulation for sample:", i, "run:", r
+                # Just exit in the child. No need to do anything else.
+                sys.exit(0)
+            else: # in parent
+                # Append the child's PID and fast forward to the next point
+                pids.append(pid)
 
     print "Waiting for children...", pids
     while pids:
@@ -252,16 +176,21 @@ if __name__ == "__m5_main__":
     # create the system we are going to simulate
     system = MySystem(opts)
 
-    if not (len(args) == 1 or len(args) == 2):
+    if not (len(args) == 1 or len(args) == 2 or len(args) == 3):
         SimpleOpts.print_help()
-        fatal("Simulate script requires one or two arguments")
+        fatal("Simulate script requires one or two or three arguments")
 
     roiInstructions = int(args[0])
 
-    if len(args) == 2:
+    if len(args) == 2 or len(args) == 3:
         samples = int(args[1])
     else:
         samples = 1
+
+    if len(args) == 3:
+        runs = int(args[2])
+    else:
+        runs = 1
 
     # For workitems to work correctly
     # This will cause the simulator to exit simulation when the first work
@@ -301,8 +230,7 @@ if __name__ == "__m5_main__":
         print "Exited because", exit_event.getCause()
 
         if exit_event.getCause() == "work started count reach":
-            simulateROI(system, roiInstructions, samples)
-            break       #  exit after ROI is complete
+            simulateROI(system, roiInstructions, samples, runs)
         elif exit_event.getCause() == "work items exit count reached":
             end_tick = m5.curTick()
 
